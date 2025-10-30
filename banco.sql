2fa24a4f9ffe3b69ca2f49f3f01fcfb9b24c6d50
italo <italo0205play@gmail.com>
2025-10-30 18:00:00 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS usuarios_db;
+CREATE DATABASE usuarios_db;
+USE usuarios_db;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE despesas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(100),
+    valor DECIMAL(10,2),
+    data_vencimento DATE
+);
